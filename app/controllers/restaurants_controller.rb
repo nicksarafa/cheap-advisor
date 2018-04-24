500309b2b4159cddc6c4a258782ccf04cd0407d8
Nick Sarafa <nicksarafa@gmail.com>
2018-04-24 16:45:56 -0500

    instantiate the RESTAURANTS fake database and connect it to the restaurants view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,21 @@
 class RestaurantsController < ApplicationController
+
+  # fake database with restaurants
+  RESTAURANTS = [
+    {
+      name: "Los Dos Hombres",
+      address: "48 Tulum BLVD",
+      category: "Mexican"
+    },
+    {
+      name: "Tres Amigos",
+      address: "58 Tulum BLVD",
+      category: "Mexican"
+    },
+  ]
+
   def index
+    # initialize database to the restaurants controller to pass to the view
+    @restaurants = RESTAURANTS
   end
 end
