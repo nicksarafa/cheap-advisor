ef2b0a66b12228438b761098a9d482fcacde7c05
Nick Sarafa <nicksarafa@gmail.com>
2018-04-24 16:40:07 -0500

    move categories into controller @instance variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,5 +10,8 @@ class PagesController < ApplicationController
   # create the home route connecting to the routes file manually
   def home
     puts "welcome home, obi one"
+
+    # define an instance variable by prepending `@`
+    @categories = %w(mexican sushi italian)
   end
 end
